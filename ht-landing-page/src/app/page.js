4d8eadf827ec7f4d8b8052fe8131c4d045c842e2
Fábio Rabello <111283847+FabioRR235@users.noreply.github.com>
2026-09-23 22:10:12 -0300

    chore: new colour palletes

--- ht-landing-page/src/app/page.js
+++ ht-landing-page/src/app/page.js
@@ -1,8 +1,12 @@
-
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <div className="bg-background text-foreground">
+      <button className="bg-primary text-primary-foreground">
+        <span className="text-accent">Teste</span>
+      </button>
+      <div className="bg-secondary">
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      </div>
+    </div>
+  );
 }
